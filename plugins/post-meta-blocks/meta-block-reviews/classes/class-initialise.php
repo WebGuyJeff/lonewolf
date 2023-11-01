06f2a1b1c274c1127b4616ef72625e2ed7bcaec2
BigupJeff <me@jeffersonreal.uk>
2023-11-01 23:23:32 +0000

    flatten init __construct if logic

--- plugins/post-meta-blocks/meta-block-reviews/classes/class-initialise.php
+++ plugins/post-meta-blocks/meta-block-reviews/classes/class-initialise.php
@@ -24,28 +24,29 @@ class Initialise {
 	 */
 	public function __construct() {
 		$def = $this->get_definition();
-		if ( is_array( $def ) && array_key_exists( 'key', $def ) ) {
 
-			$cpt = new Custom_Post_Type( $def );
-			add_action( 'init', array( $cpt, 'register' ), 0, 1 );
-
-			if ( array_key_exists( 'customFields', $def ) ) {
-
-				$classic = new Meta_Box_Classic( $def );
-				add_action( 'do_meta_boxes', array( &$classic, 'remove_default_meta_box' ), 10, 3 );
-				add_action( 'add_meta_boxes', array( &$classic, 'add_custom_meta_box' ), 10, 0 );
-				add_action( 'save_post', array( &$classic, 'save_custom_meta_box_data' ), 1, 2 );
-
-				$gutenberg = new Meta_Box_Gutenberg( $def );
-				add_action( 'init', array( &$gutenberg, 'register_gutenberg_block' ), 10, 0 );
-				add_action( 'init', array( &$gutenberg, 'register_metafields' ), 10, 0 );
-
-				// Enable WP custom fields even if ACF is installed.
-				add_filter( 'acf/settings/remove_wp_meta_box', '__return_false' );
-			}
-		} else {
+		if ( ! is_array( $def ) || ! array_key_exists( 'key', $def ) ) {
 			return;
 		}
+
+		$cpt = new Custom_Post_Type( $def );
+		add_action( 'init', array( $cpt, 'register' ), 0, 1 );
+
+		if ( ! array_key_exists( 'customFields', $def ) ) {
+			return;
+		}
+
+		$classic = new Meta_Box_Classic( $def );
+		add_action( 'do_meta_boxes', array( &$classic, 'remove_default_meta_box' ), 10, 3 );
+		add_action( 'add_meta_boxes', array( &$classic, 'add_custom_meta_box' ), 10, 0 );
+		add_action( 'save_post', array( &$classic, 'save_custom_meta_box_data' ), 1, 2 );
+
+		$gutenberg = new Meta_Box_Gutenberg( $def );
+		add_action( 'init', array( &$gutenberg, 'register_gutenberg_block' ), 10, 0 );
+		add_action( 'init', array( &$gutenberg, 'register_metafields' ), 10, 0 );
+
+		// Enable WP custom fields even if ACF is installed.
+		add_filter( 'acf/settings/remove_wp_meta_box', '__return_false' );
 	}
 
 
