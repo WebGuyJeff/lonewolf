a831e13b5ab34e64a9aa3b2ad3f7570967044858
BigupJeff <me@jeffersonreal.uk>
2023-11-14 03:00:20 +0000

    make animation responsive

--- build/blocks/camper-animation/camper-animation.asset.php
+++ build/blocks/camper-animation/camper-animation.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-i18n'), 'version' => '3c723616c4f1f0f38f5d');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-i18n'), 'version' => 'b8d29563f6a3503ca684');
